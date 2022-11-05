cd4c6cf8e97b98cc09c3bf374ad5f55ec2d34734
Nils Druyen <nils.druyen@fressnapf.com>
2022-11-05 17:05:23 +0100

    Reformat code

--- src/main/kotlin/de/nilsdruyen/gradle/ftp/UploadExtension.kt
+++ src/main/kotlin/de/nilsdruyen/gradle/ftp/UploadExtension.kt
@@ -1,10 +1,10 @@
 package de.nilsdruyen.gradle.ftp
 
 open class UploadExtension {
-  var host = ""
-  var port = 22
-  var username = ""
-  var password = ""
-  var sourceDir = ""
-  var targetDir = ""
+    var host = ""
+    var port = 22
+    var username = ""
+    var password = ""
+    var sourceDir = ""
+    var targetDir = ""
 }
