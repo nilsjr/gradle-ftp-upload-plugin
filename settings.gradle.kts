0e7e8b8c78d69132370f4202895676688b96d3a3
Nils Druyen <info@nilsdruyen.de>
2023-04-17 22:27:37 +0200

    chore: bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "gradle-ftp-upload-plugin"
